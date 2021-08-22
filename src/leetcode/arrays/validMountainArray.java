b612f4d2c004684eca934e7b3684e452e2f87c3b
huckjuhwang <skygurwn96@naver.com>
2021-08-22 17:20:47 +0900

    update vaild MountainArray problem

--- src/leetcode/arrays/validMountainArray.java
+++ src/leetcode/arrays/validMountainArray.java
@@ -3,21 +3,17 @@ package leetcode.arrays;
 public class validMountainArray {
 
     public static void main(String[] args) {
-        System.out.println(validMountainArray.validMountainArray( new int[]{2,1,2,3,5,7,9,10,12,14,15,16,18,14,13} ));
+        System.out.println(validMountainArray.validMountainArray( new int[]{1,2,3,5,7,9,10,12,14,15,16,18,14,13,15} ));
     }
     public static boolean validMountainArray(int[] arr) {
         boolean mount = true;
         int max = -1;
-
         // 꼭대기 찾는 과정
         for ( int i =0; i<arr.length; i++)
             if( arr[i] > max ) max = arr[i];
 
-        // 오르막길 밖에 없는 경우 && 내리막길 밖에 없는 경우
-        if( max == arr[arr.length-1] ||  max == arr[0] ) return false;
-
-        if( arr[0] > arr[1] ) return false;
-
+        // 오르막길 밖에 없는 경우 && 내리막길 밖에 없는 경우 && 하나만 있는 경우 && 처음에 내려가는 경우 처리
+        if( max == arr[arr.length-1] ||  max == arr[0] || arr[0] > arr[1]) return false;
 
         for( int i =1; i<arr.length -1 ; i++ ){
             // 정상이면 false 로
