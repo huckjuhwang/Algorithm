bdbf23c1d0d153e1b272898affe8df74845aefd5
huckjuhwang <skygurwn96@naver.com>
2022-03-07 22:00:44 +0900

    Q. UDLR

--- src/programmers/hash/phoneNumber.java
+++ src/programmers/hash/phoneNumber.java
@@ -1,24 +1,43 @@
 package programmers.hash;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class phoneNumber {
 
     public static void main(String[] args) {
         phoneNumber pn = new phoneNumber();
-        boolean solution = pn.solution(new String[]{"3", "32", "324"});
-
+        int solution = pn.solution(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}});
         System.out.println("solution = " + solution);
     }
 
-    public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-        for ( int i=0; i<phone_book.length-1; i++){
-            if ( phone_book[i+1].startsWith(phone_book[i]) ){
-                return false;
+    public int solution(String[][] clothes) {
+        Map<String, Integer> hm = getHashMap(clothes);
+        return getResult(hm);
+    }
+
+    private int getResult(Map<String, Integer> hm) {
+        Set<String> keySet = hm.keySet();
+        int result = 1;
+        for( String key : keySet ) {
+            result *= hm.get(key)+1;
+        }
+        return result-1;
+    }
+
+    private Map<String, Integer> getHashMap(String[][] clothes) {
+        Map<String, Integer> hm = new HashMap<>();
+        for (String clo[] : clothes){
+            String key = clo[1];
+            if( hm.get(key) == null ){
+                hm.put(key, 1);
+            }else {
+                hm.put(clo[1], hm.get(key)+1);
             }
         }
-        return true;
+        return hm;
     }
 
 }
