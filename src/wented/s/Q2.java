6f7ea32772544df1c0e17ea289e2b5b4a5084dc8
huckjuhwang <skygurwn96@naver.com>
2022-05-31 15:57:56 +0900

    위상정렬 풀이 변경 #1

--- src/wented/s/Q2.java
+++ src/wented/s/Q2.java
@@ -1,95 +1,91 @@
 package wented.s;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q2 {
-
-    static int arr[][];
-    static List<Integer> list;
-    static boolean check[];
     static int n;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int t = Integer.parseInt(br.readLine());
+
+
         for (int testCase = 1; testCase <= t; testCase++) {
             n = Integer.parseInt(br.readLine());
+            // 그래프 형태 구축
+            ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+            for (int i = 0; i <= n; i++)
+                graph.add(new ArrayList<>());
 
-            arr = new int[n + 1][n + 1];
-            check = new boolean[n + 1];
-            check[0] = true;
-            list = new ArrayList<>();
-
-            int depth = 1;
-
-            for (int i = 1; i <= n; i++) {
+            // 선행작업 테이블 생성
+            int count[] = new int[n + 1];
+            for (int from = 1; from <= n; from++) {
                 StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-                // 선수과목 개수,
-                int sunsu = Integer.parseInt(st.nextToken());
-                if (sunsu == 0) {
-                    list.add(i);
-                    check[i] = true;
-                }
-                for (int j = 0; j < sunsu; j++) {
-                    arr[i][Integer.parseInt(st.nextToken())]++;
+                // 관계 개수
+                int m = Integer.parseInt(st.nextToken());
+
+                // 각 관계에 대해서 선행작업 테이블 갱신 / 그래프 갱신
+                for (int j = 0; j < m; j++) {
+                    int to = Integer.parseInt(st.nextToken());
+                    graph.get(to).add(from);
+                    count[from]++;
                 }
             }
 
-            while (true) {
-                // 모든 선수 과목을 마쳤다면, 종료
-                if( validCheck() ) break;
+            // 선행작업이 없는 노드 queue 삽입
+            Queue<Point> q = new LinkedList<>();
+            for (int i = 1; i <= n; i++) {
+                if (count[i] == 0)  q.add(new Point(1, i));
+            }
 
-                if (depth > n) {
-                    depth = -1;
+            int depth = -1;
+            while (!q.isEmpty()) {
+                Point temp = q.poll();
+
+                ArrayList<Integer> list = graph.get(temp.value);
+                for (int i = 0; i < list.size(); i++) {
+                    int num = list.get(i);
+
+                    // 선행작업 테이블을 갱신해줌
+                    count[num]--;
+                    if (count[num] == 0) {
+                        q.add(new Point(temp.depth+1, num));
+                        depth = temp.depth + 1;
+                    }
+                }
+            }
+
+
+            // 루프여부 체크
+            boolean loop = false;
+            for (int i = 1; i <= n; i++) {
+                if( count[i] != 0 ){
+                    loop = true;
                     break;
                 }
-
-                for (int i = 1; i <= n; i++) {
-                    // true 인것을 대상으로 순회
-                    if (check[i]) {
-                        for (int j = 1; j <= n; j++) {
-                            if (arr[j][i] == 1) {
-                                arr[j][i] = 0;
-                            }
-                        }
-                    }
-
-                }
-                for (int j = 1; j <= n; j++) {
-                    boolean ch = true;
-                    if (!check[j]) {
-                        for (int k = 1; k <= n; k++) {
-                            if (arr[j][k] == 1) {
-                                ch = false;
-                            }
-                        }
-                        check[j] = ch;
-                    }
-                }
-                depth++;
-
-
             }
 
-
-            System.out.println("#" + testCase + " " + depth);
-
-
+            // 루프일 경우 -1 출력
+            if (!loop) {
+                System.out.println("#" + testCase + " " + depth);
+            }else{
+                System.out.println("#" + testCase + " " + -1);
+            }
         }
     }
 
-    public static boolean validCheck(){
-        for (int i = 1; i <= n; i++) {
-            if( !check[i] ) return false;
-        }
-        return true;
+
+}
+
+class Point{
+    public int depth;       // 현재노드의 depth
+    public int value;       // 선행 작업의 개수
+
+    public Point(int depth, int value) {
+        this.depth = depth;
+        this.value = value;
     }
 }
